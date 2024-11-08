7b3d67215eaa7d9ed3f14a5f3ecd384ddb90ae47
Cabon <bear110225@gmail.com>
2024-11-09 05:54:06 +0800

    make start screen

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 41, 5, 84),
+                Color.fromARGB(255, 66, 15, 123),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
